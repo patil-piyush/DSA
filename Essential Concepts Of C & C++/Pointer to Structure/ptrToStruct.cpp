c8218da9a962b0a36d5c9f7c9142cd7fa57e9bcc
Piyush <piyushpatil0031@gmail.com>
2024-06-17 22:53:22 +0530

    pointers to structure is completed

---
+++ Essential Concepts Of C & C++/Pointer to Structure/ptrToStruct.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+struct Rectangle{
+    int length;
+    int breadth;
+};
+
+int main(){
+    struct Rectangle r = {10 , 5};
+    struct Rectangle *p = &r; // pointer declaration + initialization
+
+    // accessing data members by using '.' operator
+    cout<<r.length<<endl;
+    cout << r.breadth << endl;
+
+    // accessing data members by using a pointer
+    cout<<p->length<<endl;
+    cout<<p->breadth<<endl;
+
+    /*
+    Allocating memory in Heap (Dynamic memory allocation)
+        struct Rectangle *p;  //pointer declaration
+        p = new Rectangle;  //dynamic allocation
+
+        //accessing data members by using a pointer
+        cout<<p->length<<endl;
+        cout<<p->breadth<<endl;
+
+    */
+
+    return 0;
+}
