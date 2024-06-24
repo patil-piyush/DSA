1be93097cedbaf34c6000a5f32af6ecbea7de242
Piyush <piyushpatil0031@gmail.com>
2024-06-24 17:21:47 +0530

    sum of n natural numbers is completed

---
+++ Recursion/Problems/sumOfNatural.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+/*
+By using Formula
+    int sum(int n)
+    {
+        return n*(n-1)/2;
+    }
+*/
+
+/*
+by using loops
+    int sum(int n)
+    {
+        int s=0;
+        for (int i = 0; i <= n; i++)
+        {
+            s=s+i;
+        }
+        return s;
+    }
+*/
+
+//by using recursion
+int sum(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum(n - 1) + n;
+    }
+}
+int main()
+{
+    int a = 5;
+    cout << sum(a);
+    return 0;
+}
