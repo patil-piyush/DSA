3eed081dd619d8ec23f146791d79c70ac759ce62
Piyush <piyushpatil0031@gmail.com>
2024-06-24 16:08:46 +0530

    Indirect recursion is completed

--- Recursion/Types of recursion/TreeRecursion.cpp
+++ Recursion/Types of recursion/TreeRecursion.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void fun(int n){
-    if(n>0){
-        cout<<n;
-        fun(n-1);
-        fun(n-1);
+void fun(int n)
+{
+    if (n > 0)
+    {
+        cout << n;
+        fun(n - 1);
+        fun(n - 1);
     }
 }
-int main(){
+int main()
+{
     int a = 3;
     fun(a);
     return 0;
