611056b27cd598c3cfe9b8268367110c54189cf6
Piyush <piyushpatil0031@gmail.com>
2024-06-24 16:59:07 +0530

    Nested recursion is completed

--- Recursion/Types of recursion/NestedRecursion.cpp
+++ Recursion/Types of recursion/NestedRecursion.cpp
@@ -1,14 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int fun(int n){
-    if(n>100){
-        return (n-10);
+int fun(int n)
+{
+    if (n > 100)
+    {
+        return (n - 10);
     }
-    else{
-        return fun(fun(n+11));
+    else
+    {
+        return fun(fun(n + 11));
     }
 }
-int main(){
-    cout<<fun(95);
+int main()
+{
+    cout << fun(95);
     return 0;
 }
