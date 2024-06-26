4288a9716ae1edf8c1539bd4a6597ea6bf718c12
Piyush <piyushpatil0031@gmail.com>
2024-06-26 17:12:36 +0530

    taylors series using horners rule (recursion) is completed

---
+++ Recursion/Problems/TaylorHR.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+//taylors series using horners rule (recursion)
+double e(int x, int n){
+    static double s=1;
+    if(n==0){
+        return s;
+    }
+    else{
+        s=1+(x*s/n);
+        return e(x,n-1);
+    }
+}
+int main(){
+    cout<<e(1,10);
+    return 0;
+}
