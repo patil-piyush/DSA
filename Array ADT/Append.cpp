8105546b031ab2f7dc831e94c446276a7927d953
Piyush <piyushpatil0031@gmail.com>
2024-06-28 16:36:07 +0530

    Array ADT - Append is completed

---
+++ Array ADT/Append.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int *A;
+    int size;
+    int length;
+};
+
+void display(struct Array a)
+{
+    for (int i = 0; i < a.length; i++)
+    {
+        cout << a.A[i] << " ";
+    }
+    cout<<endl;
+}
+
+void append(struct Array *arr, int x){
+    if (arr->length < arr->size)
+    {
+        arr->A[arr->length] = x;
+        arr->length++;
+    }
+}
+int main()
+{
+    struct Array arr;
+    cout << "Enter the size: ";
+    cin >> arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    int n;
+    cout << "enter the number of elements: ";
+    cin >> n;
+    cout << "enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr.A[i];
+    }
+    arr.length = n;
+
+    display(arr);
+
+    append(&arr,10);
+
+    display(arr);
+
+    return 0;
+}
