26d69fbfb79b46407810b65846b1077ea89e58d0
Piyush <piyushpatil0031@gmail.com>
2024-07-07 15:59:58 +0530

    Array ADT - average Functions are completed

---
+++ Array ADT/Average.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int *A;
+    int size;
+    int length;
+};
+
+void display(struct Array a)
+{
+    for (int i = 0; i < a.length; i++)
+    {
+        cout << a.A[i] << " ";
+    }
+    cout << endl;
+}
+
+// normal average function
+int average(struct Array arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++)
+    {
+        sum = sum + arr.A[i];
+    }
+    return sum/arr.length;
+}
+
+
+int main()
+{
+    struct Array arr;
+    cout << "Enter the size: ";
+    cin >> arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    int n;
+    cout << "enter the number of elements: ";
+    cin >> n;
+    cout << "enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr.A[i];
+    }
+    arr.length = n;
+
+    display(arr);
+
+    // calling average function
+    cout << average(arr) << endl;
+    return 0;
+}
