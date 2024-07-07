0feca1f65b6436d9e99621302a9de157b2675398
Piyush <piyushpatil0031@gmail.com>
2024-07-07 22:37:20 +0530

    Array ADT - Insertion in asorted array is completed

---
+++ Array ADT/InsertInSortedArray.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int *A;
+    int size;
+    int length;
+};
+
+// Normal insert function
+void Insert(struct Array *arr, int x)
+{
+        int i= arr->length-1;
+        while(arr->A[i]>x){
+            arr->A[i+1] = arr->A[i];
+            i--;
+        }
+        arr->A[i+1] = x;
+        arr->length++;
+}
+
+void display(struct Array a)
+{
+    for (int i = 0; i < a.length; i++)
+    {
+        cout << a.A[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    struct Array arr;
+    cout << "Enter the size: ";
+    cin >> arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    int n;
+    cout << "enter the number of elements: ";
+    cin >> n;
+    cout << "enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr.A[i];
+    }
+    arr.length = n;
+
+    display(arr);
+
+    // calling insert function for sorted array
+    Insert(&arr, 10);
+
+    display(arr);
+
+    return 0;
+}
