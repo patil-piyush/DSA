4a0cefd923bcf491ef4c9fd5999fd3fb9566de4c
Piyush <piyushpatil0031@gmail.com>
2024-07-07 23:24:54 +0530

    Array ADT - union for sorted array is completed

---
+++ Array ADT/Set Operations/For sorted arrays/Union.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int *A;
+    int size;
+    int length;
+};
+
+void display(struct Array a)
+{
+    for (int i = 0; i < a.length; i++)
+    {
+        cout << a.A[i] << " ";
+    }
+    cout << endl;
+}
+
+// function for Union by using merging
+void Union(struct Array first, struct Array second, struct Array *third)
+{
+    third->size = first.size + second.size;
+    third->length = 0;
+    int i, j, k;
+    i = j = k = 0;
+    while (i < first.length && j < second.length)
+    {
+        if (first.A[i] > second.A[j])
+        {
+            third->A[k++] = second.A[j++];
+        }
+        else if (first.A[i] < second.A[j])
+        {
+            third->A[k++] = first.A[i++];
+        }
+        else
+        {
+            third->A[k++] = second.A[j++];
+            i++;
+        }
+    }
+
+    for (; i < first.length; i++)
+    {
+        third->A[k++] = first.A[i];
+    }
+    for (; j < second.length; j++)
+    {
+        third->A[k++] = second.A[j];
+    }
+    third->length = k;
+}
+
+int main()
+{
+    // taking input for First array
+    struct Array first;
+    cout << "Enter the size: ";
+    cin >> first.size;
+    first.A = new int[first.size];
+    first.length = 0;
+
+    int n;
+    cout << "enter the number of elements: ";
+    cin >> n;
+    cout << "enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> first.A[i];
+    }
+    first.length = n;
+
+    // taking input for second array
+    struct Array second;
+    cout << "Enter the size: ";
+    cin >> second.size;
+    second.A = new int[second.size];
+    second.length = 0;
+
+    cout << "enter the number of elements: ";
+    cin >> n;
+    cout << "enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> second.A[i];
+    }
+    second.length = n;
+
+    struct Array third;
+    Union(first, second, &third);
+    display(third);
+
+    return 0;
+}
