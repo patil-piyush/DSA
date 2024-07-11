8f798cf72b2fc746d69a34ae0cf3f3d645c306c9
Piyush <piyushpatil0031@gmail.com>
2024-07-12 02:27:30 +0530

    Array ADT - multiple missing element in an sorted array is done

--- Array ADT/Other Operations/Multiple_Missing_Element_Sorted.cpp
+++ Array ADT/Other Operations/Multiple_Missing_Element_Sorted.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main(){
-    
+    int n = 11;
+    int a[n] = {6,7,8,9,11,12,15,16,17,18,19};
+    int l = a[0];
+    int diff = l - 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] - i != diff)
+        {
+            while (diff<a[i]-i)
+            {
+                cout<<i+diff;
+                diff++;
+            }
+            
+        }
+    }
     return 0;
 }
