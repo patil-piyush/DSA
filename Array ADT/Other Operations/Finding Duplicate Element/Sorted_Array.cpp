53ceec1567df3754658b8fcb0e438f3acba7cbaa
Piyush <piyushpatil0031@gmail.com>
2024-07-12 02:44:52 +0530

    Array ADT - Duplicate element in an sorted array is done

--- Array ADT/Other Operations/Finding Duplicate Element/Sorted_Array.cpp
+++ Array ADT/Other Operations/Finding Duplicate Element/Sorted_Array.cpp
@@ -1,14 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n = 11;
     int a[n] = {6, 7, 8, 9, 11, 12, 15, 16, 17, 18, 19};
+
+    // method 1 - bu simple checking
     int LastDuplicate = 0;
-    for(int i=0; i<n; i++){
-        if(LastDuplicate != a[i] && a[i] == a[i+1]){
-            cout<<a[i]<<endl;
-            LastDuplicate=a[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (LastDuplicate != a[i] && a[i] == a[i + 1])
+        {
+            cout << a[i] << endl;
+            LastDuplicate = a[i];
+        }
+    }
+
+    // counting
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] == a[i + 1])
+        {
+            int j = i + 1;
+            while (a[j] == a[i])
+            {
+                j++;
+            }
+            cout << a[i] << " " << j - i;
+            i = j - 1;
+        }
+    }
+
+    // method 2 - by hashing
+    int h = a[n - 1], l = a[0];
+    int H[h] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        H[a[i]]++;
+    }
+    for (int i = l; i < h; i++)
+    {
+        if (H[i] > 0)
+        {
+            cout << i << " " << H[i] << endl;
         }
     }
     return 0;
---
+++ Array ADT/Other Operations/Finding Duplicate Element/Unsorted_Array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 11;
+    int a[n] = {6, 7, 8, 9, 11, 12, 15, 16, 17, 18, 19};
+    int LastDuplicate = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (LastDuplicate != a[i] && a[i] == a[i + 1])
+        {
+            cout << a[i] << endl;
+            LastDuplicate = a[i];
+        }
+    }
+    return 0;
+}
