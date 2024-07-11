b6fe4af9867bc7e513168a4be2fef154c2f65881
Piyush <piyushpatil0031@gmail.com>
2024-07-12 02:23:51 +0530

    Array ADT - single missing element in an sorted array is done

--- Array ADT/Other Operations/Single_Missing_Element_Sorted.cpp
+++ Array ADT/Other Operations/Single_Missing_Element_Sorted.cpp
@@ -1,17 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    //this method is only applicable for missing element in an array of sequence of first n natural numbers
-    int sum=0,n=11;
-    int a[n] = {1,2,3,4,5,6,8,9,10,11,12};
+    int sum = 0, n = 11;
+    int a[n] = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12};
+    // Method 1
+    // this method is only applicable for missing element in an array of sequence of first n natural numbers
     for (int i = 0; i < n; i++)
     {
         sum = sum + a[i];
     }
-    int s = n*(n+1)/2;
-    cout<<"Missing element is "<<s-sum; 
-    
+    int s = n * (n + 1) / 2;
+    cout << "Missing element is " << s - sum;
+
+    // method 2
+    // this method is applicable for any sorted array
+    int l = a[0];
+    int diff = l - 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] - i != diff)
+        {
+            cout << "Missing element is " << i + diff;
+            break;
+        }
+    }
+
     return 0;
 }
