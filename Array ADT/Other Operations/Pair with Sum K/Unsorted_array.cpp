1510124a8e918ee25b7644aa910f18b90473be47
Piyush <piyushpatil0031@gmail.com>
2024-07-12 12:13:30 +0530

    Array ADT - finding pair with sum k in an unsorted array is done

---
+++ Array ADT/Other Operations/Pair with Sum K/Unsorted_array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 10,k=10;
+    int a[n] = {6, 3, 8, 10, 16, 7, 5, 2, 9, 14};
+    //method 1 - simply by using 2 for loop
+    for (int i = 0; i < n-1; i++)
+    {
+        for(int j=i+1; j<n; j++){
+            if(a[i]+a[j]==k){
+                cout<<a[i]<<" "<<a[j]<<" "<<k<<endl;
+            }
+        }
+    }
+
+    //method 2 - by using hashing
+    int H[16]={0};
+    for(int i=0; i<n; i++){
+        if(H[k-a[i]] != 0){
+            cout<<a[i]<<" "<<k-a[i]<<" "<<k<<endl;
+        }
+        H[a[i]]++;
+    }
+    
+    return 0;
+}
