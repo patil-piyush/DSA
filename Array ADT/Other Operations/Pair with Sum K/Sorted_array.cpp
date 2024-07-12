67499fb6f4d4a50abf3b314978680beb93d6c6a9
Piyush <piyushpatil0031@gmail.com>
2024-07-12 12:17:56 +0530

    Array ADT - finding pair with sum k in an sorted array is done

--- Array ADT/Other Operations/Pair with Sum K/Sorted_array.cpp
+++ Array ADT/Other Operations/Pair with Sum K/Sorted_array.cpp
@@ -4,19 +4,23 @@ using namespace std;
 int main()
 {
     int n = 10, k = 10;
-    int a[n] = {1,3,4,5,6,8,9,10,12,14};
+    int a[n] = {1, 3, 4, 5, 6, 8, 9, 10, 12, 14};
 
-    int i=0,j=n-1;
-    while(i<j){
-        if(a[i]+a[j]==k){
-            cout<<a[i]<<" "<<a[j]<<" "<<k<<endl;
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        if (a[i] + a[j] == k)
+        {
+            cout << a[i] << " " << a[j] << " " << k << endl;
             i++;
             j--;
         }
-        else if(a[i]+a[j]<k){
+        else if (a[i] + a[j] < k)
+        {
             i++;
         }
-        else{
+        else
+        {
             j--;
         }
     }
