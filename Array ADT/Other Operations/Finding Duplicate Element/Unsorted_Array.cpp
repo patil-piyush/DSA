c72a8d66837e28578f91766f1371dbd72cb2bab0
Piyush <piyushpatil0031@gmail.com>
2024-07-12 12:18:40 +0530

    Array ADT - Duplicate element in an unsorted array is done

--- Array ADT/Other Operations/Finding Duplicate Element/Unsorted_Array.cpp
+++ Array ADT/Other Operations/Finding Duplicate Element/Unsorted_Array.cpp
@@ -6,22 +6,23 @@ int main()
     int n = 11;
     int a[n] = {6, 7, 8, 9, 11, 12, 15, 16, 17, 18, 19};
     int count = 0;
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         count = 1;
         if (a[i] != -1)
         {
-            for (int j = i+1; i < n; j++)
+            for (int j = i + 1; i < n; j++)
             {
-                if(a[i]==a[j]){
+                if (a[i] == a[j])
+                {
                     count++;
-                    a[j]=-1;
+                    a[j] = -1;
                 }
             }
-            if(count>1){
-                cout<<a[i]<<" "<<count<<endl;
+            if (count > 1)
+            {
+                cout << a[i] << " " << count << endl;
             }
-            
         }
     }
     return 0;
