0366e205b2ae5e8729f9a87074db157363aaa014
Piyush <piyushpatil0031@gmail.com>
2024-07-14 17:17:39 +0530

    String - changing case is done

--- Strings/Changing_Case.cpp
+++ Strings/Changing_Case.cpp
@@ -1,7 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    char s[10];
+    cin >> s;
+
+    // for uppercase to lowercase
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] >= 65 && s[i] <= 90)
+        {
+            s[i] = s[i] + 32;
+        }
+    }
+    cout << s;
+
+    // for lowercase to uppercase
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] >= 97 && s[i] <= 122)
+        {
+            s[i] = s[i] - 32;
+        }
+    }
+    cout << s;
+
     return 0;
 }
