2a326c101f5ac6d5a334c374656142aecbe6792a
Piyush <piyushpatil0031@gmail.com>
2024-07-14 17:53:01 +0530

    String - Reversing string - method 2 is done

--- Strings/Reversing_String.cpp
+++ Strings/Reversing_String.cpp
@@ -1,35 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    char a[10]="python";
-    int i=0,j=0;
+int main()
+{
+    char a[10] = "python";
+    int i = 0, j = 0;
 
-    //method 1 - using extra array
+    // method 1 - using extra array
     char b[10];
-    for (i = 0; a[i] != '\0'; i++){}
-    i=i-1;
-
-    for (j = 0; i>=0; j++,i--)
+    for (i = 0; a[i] != '\0'; i++)
     {
-        b[j]=a[i];
     }
-    b[j]='\0';
-    cout<<b<<endl;
+    i = i - 1;
+
+    for (j = 0; i >= 0; j++, i--)
+    {
+        b[j] = a[i];
+    }
+    b[j] = '\0';
+    cout << b << endl;
 
     // method 2 - without using extra array
-    i=j=0;
+    i = j = 0;
     char c[10] = "python";
-    for (i = 0; a[i] != '\0'; i++){}
+    for (i = 0; a[i] != '\0'; i++)
+    {
+    }
     i = i - 1;
     char temp;
-    for (j = 0; j<i; j++, i--)
-    {   
+    for (j = 0; j < i; j++, i--)
+    {
         temp = c[j];
         c[j] = c[i];
         c[i] = temp;
     }
-    cout << c <<endl;
+    cout << c << endl;
 
     return 0;
 }
