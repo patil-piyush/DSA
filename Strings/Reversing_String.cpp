aa05224c1c2944b617e0422bbfa13da6b1cc4d63
Piyush <piyushpatil0031@gmail.com>
2024-07-14 17:52:53 +0530

    String - Reversing string - method 2 is done

--- Strings/Reversing_String.cpp
+++ Strings/Reversing_String.cpp
@@ -15,8 +15,21 @@ int main(){
         b[j]=a[i];
     }
     b[j]='\0';
-    cout<<b;
+    cout<<b<<endl;
 
+    // method 2 - without using extra array
+    i=j=0;
+    char c[10] = "python";
+    for (i = 0; a[i] != '\0'; i++){}
+    i = i - 1;
+    char temp;
+    for (j = 0; j<i; j++, i--)
+    {   
+        temp = c[j];
+        c[j] = c[i];
+        c[i] = temp;
+    }
+    cout << c <<endl;
 
     return 0;
 }
