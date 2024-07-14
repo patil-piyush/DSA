69583c6bf874395dda2d8a126691596d000a55e2
Piyush <piyushpatil0031@gmail.com>
2024-07-14 18:21:23 +0530

    String - Comparing strings is done

--- Strings/Comparing_Strings.cpp
+++ Strings/Comparing_Strings.cpp
@@ -6,7 +6,27 @@ int main()
     char a[] = "Piyush";
     char b[] = "Piyush";
     int i = 0, j = 0;
-    
+
+    // if case is different than change case of both the strings
+    //  for uppercase to lowercase
+    //  changing case of string a
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        if (a[i] >= 65 && a[i] <= 90)
+        {
+            a[i] = a[i] + 32;
+        }
+    }
+    // changing case of string b
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] >= 65 && b[i] <= 90)
+        {
+            b[i] = b[i] + 32;
+        }
+    }
+
+    // comparing strings
     for (i = 0, j = 0; a[i] != '\0' && b[j] != '\0'; i++, j++)
     {
         if (a[i] != b[j])
