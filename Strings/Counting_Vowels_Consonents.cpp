c8269d1cfe5828473256b2db8bf2b6b30a9246bc
Piyush <piyushpatil0031@gmail.com>
2024-07-14 17:27:21 +0530

    String - changing case is done

--- Strings/Counting_Vowels_Consonents.cpp
+++ Strings/Counting_Vowels_Consonents.cpp
@@ -7,13 +7,14 @@ int main()
     int Vcount = 0, Ccount = 0;
     cin >> s;
 
-    // for uppercase to lowercase
     for (int i = 0; s[i] != '\0'; i++)
     {
+        //for vowels
         if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
         {
             Vcount++;
         }
+        //for consonents
         else if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
         {
             Ccount++;
