b68da55467fc2636af111c5a210470f37f8ac6ce
Piyush <piyushpatil0031@gmail.com>
2024-07-14 17:32:06 +0530

    String - counting Words is done

--- Strings/Counting_Words.cpp
+++ Strings/Counting_Words.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main()
 {
-    char s[]="How  are you?";
+    char s[] = "How  are you?";
     int count = 1;
 
-
-    //if there are no white spaces
+    // if there are no white spaces
     for (int i = 0; s[i] != '\0'; i++)
     {
-       if(s[i]==' '){
-        count++;
-       }
+        if (s[i] == ' ')
+        {
+            count++;
+        }
     }
 
     // if there are white spaces
     for (int i = 0; s[i] != '\0'; i++)
     {
-        if (s[i] == ' ' && s[i-1]!=' ')
+        if (s[i] == ' ' && s[i - 1] != ' ')
         {
             count++;
         }
