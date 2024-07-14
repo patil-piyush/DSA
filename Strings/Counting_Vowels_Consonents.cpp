9ff801cd4f94e0fa0d8d3cb3764aba91d018fba5
Piyush <piyushpatil0031@gmail.com>
2024-07-14 17:25:46 +0530

    String - counting vowels and consonents is done

---
+++ Strings/Counting_Vowels_Consonents.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char s[10];
+    int Vcount = 0, Ccount = 0;
+    cin >> s;
+
+    // for uppercase to lowercase
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+        {
+            Vcount++;
+        }
+        else if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+        {
+            Ccount++;
+        }
+    }
+
+    cout << "vowels: " << Vcount << endl;
+    cout << "consonents: " << Ccount << endl;
+    return 0;
+}
