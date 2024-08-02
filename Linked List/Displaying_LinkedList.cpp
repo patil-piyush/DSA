9da8230f7a024d55820c3310dd404e9fab59a319
Piyush Patil <piyushpatil0031@gmail.com>
2024-08-02 22:17:44 +0530

    Displaying linked list using a recursive function is done

--- Linked List/Displaying_LinkedList.cpp
+++ Linked List/Displaying_LinkedList.cpp
@@ -1,49 +1,55 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-struct Node{
+struct Node
+{
     int data;
-    struct Node*next;
-}*first;
+    struct Node *next;
+} *first;
 
-void create(int a[], int n){
+void create(int a[], int n)
+{
     int i;
-    struct Node *t,*last;
+    struct Node *t, *last;
     first = new Node;
-    first->data=a[0];
-    first->next=0;
-    last=first;
+    first->data = a[0];
+    first->next = 0;
+    last = first;
 
-    for(i=1; i<n; i++){
+    for (i = 1; i < n; i++)
+    {
         t = new Node;
-        t->data=a[i];
-        t->next=0;
+        t->data = a[i];
+        t->next = 0;
         last->next = t;
-        last=t;
+        last = t;
     }
 }
 
-//simple display function for displaying linked list
-void DisplayLL(struct Node *p){
-    while(p!=0){
-        cout<<p->data<<endl;
-        p=p->next;
+// simple display function for displaying linked list
+void DisplayLL(struct Node *p)
+{
+    while (p != 0)
+    {
+        cout << p->data << endl;
+        p = p->next;
     }
 }
 
-//recursive display function for displaying linked list
-void RDisplayLL(struct Node *p){
-    if(p!=0){
-        cout<<p->data<<endl;
+// recursive display function for displaying linked list
+void RDisplayLL(struct Node *p)
+{
+    if (p != 0)
+    {
+        cout << p->data << endl;
         RDisplayLL(p->next);
     }
 }
 
-
-int main(){
-    int a[]= {10,20,30,40,50};
-    create(a,5);
+int main()
+{
+    int a[] = {10, 20, 30, 40, 50};
+    create(a, 5);
     RDisplayLL(first);
     return 0;
 }
